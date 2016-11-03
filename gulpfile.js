0ff6d4f485277b966124c7a9d026656237f27e0b
Ronny Rook <rook.ronny@gmail.com>
2016-11-03 21:30:27 +0100

    added normalize and changed style output name

--- gulpfile.js
+++ gulpfile.js
@@ -9,11 +9,11 @@ gulp.task('sass', function () {
 
   return gulp.src(src)
     .pipe(sass())
-    .pipe(concat('styles.css'))
+    .pipe(concat('style.css'))
     .pipe(gulp.dest('./css/'));
 });
 
 gulp.task('default', function() {
     gulp.start('sass');
-    gulp.watch('./*.scss', ['sass']);
+    gulp.watch('./scss/*.scss', ['sass']);
 });
