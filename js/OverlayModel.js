bcd575c43d2ac03d6dc357d68049e6004e365c33
Ronny Rook <rook.ronny@gmail.com>
2016-11-06 20:13:50 +0100

    add navigation between overlays

--- js/OverlayModel.js
+++ js/OverlayModel.js
@@ -1,18 +1,26 @@
 class OverlayModel {
-  constructor(data, dataOrder) {
+  constructor(data, get, dataOrder) {
+    this.get = get;
     this.data = data;
     this.dataOrder = dataOrder;
 
     this.init = this.init.bind(this);
     this.addEvents = this.addEvents.bind(this);
-    this.insertData = this.insertData.bind(this);
+    this.createPopup = this.createPopup.bind(this);
+    this.nav = this.nav.bind(this);
     this.closeOverlay = this.closeOverlay.bind(this);
+    this.insertData = this.insertData.bind(this);
 
     // Class variables
     this.overlay = document.createElement('div');
     this.bodyElement = document.querySelector('body');
 
+    // App variables
     this.fadeDuration = 1000;
+    this.isBeingAdded = false;
+
+    this.index = this.objectedIndex(data, get);
+    this.length = Object.keys(this.data).length - 1;
 
     this.init();
   }
@@ -20,17 +28,16 @@ class OverlayModel {
   init() {
     this.overlay.classList.add('overlay-model');
 
-    const popupElement = document.createElement('div');
-    popupElement.classList.add('overlay-model__popup');
-
+    // Add items to initial fade
     const closeElement = document.createElement('span');
     closeElement.classList.add('overlay-model__close', 'fa', 'fa-close');
-
     this.overlay.appendChild(closeElement);
-    this.overlay.appendChild(popupElement);
+
+    this.createPopup(this.index);
 
     // Insert hidden overlay and fade in
     this.bodyElement.insertBefore(this.overlay, this.bodyElement.firstChild);
+
     setTimeout(() => {
       this.overlay.classList.add('overlay-model--visible');
     }, 100);
@@ -38,18 +45,97 @@ class OverlayModel {
     this.addEvents();
   }
 
+  /** Add key and click events **/
   addEvents() {
     document.querySelector('.overlay-model__close').addEventListener('click', () => {
       this.closeOverlay();
     });
 
     document.onkeydown = (evt) => {
-      if (evt.keyCode === 27 && this.overlay) {
+      if (evt.keyCode === 27 && this.overlay) { // ESC
         this.closeOverlay();
+      } else if (evt.keyCode === 37 && this.overlay) { // Left
+        this.nav('left');
+      } else if (evt.keyCode === 39 && this.overlay) { // Right
+        this.nav('right');
       }
     };
+  }
 
-    this.insertData(this.data, this.dataOrder);
+  /** Add pop-up elem to overlay top/left/right **/
+  createPopup(index = false, type = null) {
+    // @TODO popup vanaf links, link laten infaden huidige rechts eruit en visa versa
+    const popupElement = document.createElement('div');
+    popupElement.classList.add('overlay-model__popup');
+
+    if(type === 'left') {
+      popupElement.classList.add('fade', 'fade--in-left');
+    } else if(type === 'right') {
+      popupElement.classList.add('fade', 'fade--in-right');
+    }
+
+    this.overlay.appendChild(popupElement);
+
+    // Get data for specific popup by index
+    const popupData = this.objectedIndex(data, index);
+    this.insertData(popupData, this.dataOrder);
+
+    if(type) {
+      this.animateElems(type);
+    }
+  }
+
+  nav(type) {
+    // Last action not finished yet
+    if(!this.isBeingAdded) {
+      this.isBeingAdded = true;
+
+      if(type == 'left') {
+        this.index =
+          this.index === 0
+            ? this.length
+            : this.index -= 1;
+      } else if(type == 'right') {
+        this.index =
+          this.index === this.length
+            ? 0
+            : this.index += 1;
+      }
+
+      this.createPopup(this.index, type);
+    }
+  }
+
+  animateElems(type) {
+    const popupElems = document.querySelectorAll('.overlay-model__popup');
+
+    popupElems.forEach(elem => {
+      if(elem.classList.contains('fade')) {
+        setTimeout(function() {
+          if(type === 'left') {
+            elem.classList.remove('fade--in-left', 'fade');
+          } else if(type === 'right') {
+            elem.classList.remove('fade--in-right', 'fade');
+          }
+        }, 200);
+      } else {
+        const direction = type === 'left'
+          ? 'fade--out-right'
+          : 'fade--out-left';
+
+        elem.classList.add('fade', direction);
+
+        // Remove elem after fading
+        setTimeout(function() {
+          this.overlay.removeChild(elem);
+        }.bind(this), this.fadeDuration);
+      }
+    });
+
+    // Can navigate again
+    setTimeout(function() {
+      this.isBeingAdded = false;
+    }.bind(this), this.fadeDuration);
   }
 
   closeOverlay() {
@@ -109,6 +195,23 @@ class OverlayModel {
       } else {
         this.overlay.lastChild.appendChild(addElements[a]).innerHTML = data[dataOrder[a]];
       }
+    }
+  }
+
+  // Return index of key or return value by index
+  objectedIndex(data, get) {
+    let i = 0;
+
+    for(let key in data) {
+      // return int, search by key
+      if(typeof get === 'string' && key == get) {
+        return i;
+      } // return object, search by index
+      else if(typeof get === 'number' && i === get) {
+        return data[key];
+      }
+
+      i++;
     }
   }
 }
